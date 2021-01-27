fd1941bd1f1f307ba4b4a16d9f519dddbaa5e319
Abednego Wambua <abedxh@gmail.com>
2021-01-27 16:28:26 +0300

    intial setup for development

--- config.py
+++ config.py
@@ -1,1 +1,27 @@
-class Co
+class Config(object):
+    """
+       Common configurations
+       """
+
+
+class DevConfig(Config):
+    """
+       Development configurations
+       """
+
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+
+
+class ProdConfig(Config):
+    """
+    Production configurations
+    """
+
+    DEBUG = False
+
+
+app_config = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
